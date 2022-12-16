583ad2dece0d4b12221cd70e359090e0c8a407a8
l-konatowski <konat1993@gmail.com>
2022-12-16 23:31:03 +0100

    configured mui5

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,22 +1,58 @@
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import * as React from 'react'
+import { useState } from 'react'
+import { RemixBrowser } from "@remix-run/react"
+import { startTransition, StrictMode } from "react"
+import { hydrateRoot } from "react-dom/client"
+
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import ClientStyleContext from './src/ClientStyleContext'
+import createEmotionCache from './src/createEmotionCache'
+import theme from './src/theme'
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = useState(createEmotionCache())
+
+  const clientStyleContextValue = React.useMemo(
+    () => ({
+      reset() {
+        setCache(createEmotionCache())
+      },
+    }),
+    [],
+  )
+
+  return (
+    <ClientStyleContext.Provider value={clientStyleContextValue}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  )
+}
 
 function hydrate() {
   startTransition(() => {
     hydrateRoot(
       document,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
-  });
+      <ClientCacheProvider>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <RemixBrowser />
+        </ThemeProvider>
+      </ClientCacheProvider>,
+    )
+  })
 }
 
 if (window.requestIdleCallback) {
-  window.requestIdleCallback(hydrate);
+  window.requestIdleCallback(hydrate)
 } else {
   // Safari doesn't support requestIdleCallback
   // https://caniuse.com/requestidlecallback
-  window.setTimeout(hydrate, 1);
+  window.setTimeout(hydrate, 1)
 }
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,11 +1,20 @@
-import { PassThrough } from "stream";
-import type { EntryContext } from "@remix-run/node";
-import { Response } from "@remix-run/node";
-import { RemixServer } from "@remix-run/react";
-import isbot from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
+import * as React from 'react'
+import { renderToString } from 'react-dom/server'
+import { PassThrough } from "stream"
+import type { EntryContext } from "@remix-run/node"
+import createEmotionCache from './src/createEmotionCache'
+import theme from './src/theme'
+import CssBaseline from '@mui/material/CssBaseline'
+import { Response } from "@remix-run/node"
+import { RemixServer } from "@remix-run/react"
+import { ThemeProvider } from '@mui/material/styles'
+import { CacheProvider } from '@emotion/react'
+import createEmotionServer from '@emotion/server/create-instance'
 
-const ABORT_DELAY = 5000;
+// import isbot from "isbot"
+import { renderToPipeableStream } from "react-dom/server"
+
+const ABORT_DELAY = 5000
 
 export default function handleRequest(
   request: Request,
@@ -13,19 +22,61 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  return isbot(request.headers.get("user-agent"))
-    ? handleBotRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        remixContext
-      )
-    : handleBrowserRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        remixContext
-      );
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  function MuiRemixServer() {
+    return (
+      <CacheProvider value={cache}>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <RemixServer context={remixContext} url={request.url} />
+        </ThemeProvider>
+      </CacheProvider>
+    )
+  }
+
+  // Render the component to a string.
+  const html = renderToString(<MuiRemixServer />)
+
+  // Grab the CSS from emotion
+  const { styles } = extractCriticalToChunks(html)
+
+  let stylesHTML = ''
+
+  styles.forEach(({ key, ids, css }) => {
+    const emotionKey = `${key} ${ids.join(' ')}`
+    const newStyleTag = `<style data-emotion="${emotionKey}">${css}</style>`
+    stylesHTML = `${stylesHTML}${newStyleTag}`
+  })
+
+  // Add the Emotion style tags after the insertion point meta tag
+  const markup = html.replace(
+    /<meta(\s)*name="emotion-insertion-point"(\s)*content="emotion-insertion-point"(\s)*\/>/,
+    `<meta name="emotion-insertion-point" content="emotion-insertion-point"/>${stylesHTML}`,
+  )
+
+  responseHeaders.set('Content-Type', 'text/html')
+
+  return new Response(`<!DOCTYPE html>${markup}`, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  });
+
+  // return isbot(request.headers.get("user-agent"))
+  //   ? handleBotRequest(
+  //     request,
+  //     responseStatusCode,
+  //     responseHeaders,
+  //     remixContext
+  //   )
+  //   : handleBrowserRequest(
+  //     request,
+  //     responseStatusCode,
+  //     responseHeaders,
+  //     remixContext
+  //   )
 }
 
 function handleBotRequest(
@@ -35,38 +86,38 @@ function handleBotRequest(
   remixContext: EntryContext
 ) {
   return new Promise((resolve, reject) => {
-    let didError = false;
+    let didError = false
 
     const { pipe, abort } = renderToPipeableStream(
       <RemixServer context={remixContext} url={request.url} />,
       {
         onAllReady() {
-          const body = new PassThrough();
+          const body = new PassThrough()
 
-          responseHeaders.set("Content-Type", "text/html");
+          responseHeaders.set("Content-Type", "text/html")
 
           resolve(
             new Response(body, {
               headers: responseHeaders,
               status: didError ? 500 : responseStatusCode,
             })
-          );
+          )
 
-          pipe(body);
+          pipe(body)
         },
         onShellError(error: unknown) {
-          reject(error);
+          reject(error)
         },
         onError(error: unknown) {
-          didError = true;
+          didError = true
 
-          console.error(error);
+          console.error(error)
         },
       }
-    );
+    )
 
-    setTimeout(abort, ABORT_DELAY);
-  });
+    setTimeout(abort, ABORT_DELAY)
+  })
 }
 
 function handleBrowserRequest(
@@ -76,36 +127,36 @@ function handleBrowserRequest(
   remixContext: EntryContext
 ) {
   return new Promise((resolve, reject) => {
-    let didError = false;
+    let didError = false
 
     const { pipe, abort } = renderToPipeableStream(
       <RemixServer context={remixContext} url={request.url} />,
       {
         onShellReady() {
-          const body = new PassThrough();
+          const body = new PassThrough()
 
-          responseHeaders.set("Content-Type", "text/html");
+          responseHeaders.set("Content-Type", "text/html")
 
           resolve(
             new Response(body, {
               headers: responseHeaders,
               status: didError ? 500 : responseStatusCode,
             })
-          );
+          )
 
-          pipe(body);
+          pipe(body)
         },
         onShellError(err: unknown) {
-          reject(err);
+          reject(err)
         },
         onError(error: unknown) {
-          didError = true;
+          didError = true
 
-          console.error(error);
+          console.error(error)
         },
       }
-    );
+    )
 
-    setTimeout(abort, ABORT_DELAY);
-  });
+    setTimeout(abort, ABORT_DELAY)
+  })
 }
